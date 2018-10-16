4aec6c435e8d11a35fd5dafca454c4db1962a118
osdevisnot <osdevisnot@gmail.com>
2018-10-16 11:31:25 -0700

    Serve library during dev builds

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,14 @@ const json = require('rollup-plugin-json')
 const typescript = require('rollup-plugin-typescript2')
 const commonjs = require('rollup-plugin-commonjs')
 const sourcemap = require('rollup-plugin-sourcemaps')
+const serve = require('rollup-plugin-serve')
+const livereload = require('rollup-plugin-livereload')
+
 const terser = require('rollup-plugin-terser').terser
 
 const pkg = require('./package.json')
+
+const isDev = !!process.env.ROLLUP_WATCH
 
 const libName = pkg.name
   .split('/')
@@ -15,7 +20,14 @@ const config = {
   input: 'src/starter-typescript-library.ts',
   external: Object.keys(pkg.dependencies),
   watch: { include: 'src/**' },
-  plugins: [json(), typescript({ useTsconfigDeclarationDir: true }), commonjs(), sourcemap()]
+  plugins: [
+    json(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    commonjs(),
+    sourcemap(),
+    isDev && serve({ contentBase: ['public', 'dist'], historyApiFallback: true, port: 4444 }),
+    isDev && livereload('dist')
+  ].filter(Boolean)
 }
 
 export default [
